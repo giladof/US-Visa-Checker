cc58a3b0dbc3c42d6f126413dc6cb00ef7226631
giladof <gilad@appsec-labs.com>
2015-03-12 15:25:56 +0200

    Visa Obf

--- VisaChecker.py
+++ VisaChecker.py
@@ -9,5 +9,5 @@ import ctypes
 conn = httplib.HTTPConnection("israel.usembassy.gov");
 conn.request("GET", "/consular/niv/checkstatus.html");
 r1 = conn.getresponse();
-if "AA004RQITW" in r1.read():
+if "[Visa]" in r1.read():
 	ctypes.windll.user32.MessageBoxA(0, "Visa found!", "US Visa", 1)
